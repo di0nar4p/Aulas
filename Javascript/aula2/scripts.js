3eaa9cfbdb84f621050ba3a211b268e2ed3ceb49
di0nar4p <glaucoplay@live.com>
2023-02-27 11:52:58 -0300

    Segunda aula de JS

--- Javascript/aula2/scripts.js
+++ Javascript/aula2/scripts.js
@@ -3,10 +3,45 @@
 //document.getElementById("teste").innerHTML = x;
 
 //Exemplo de condicional
-let numero =prompt("Digite um número");
+/*let numero =prompt("Digite um número");
         if(numero%2 == 0){
             alert(numero +" é par");
         }
         else{
             alert(numero +" é ímpar");
+        }
+            
+*/          
+            //Casting
+            //dia= Number(prompt("Dia da semana"))
+            
+            switch (new Date().getDay()) { //Retorna o dia da semana como um int de 0-6
+            case 0:
+                day = "Domingo";
+                alert(day); 
+                break;
+            case 1:
+                day = "Segunda-feira"; 
+                alert(day); 
+                break;
+            case 2:
+                day = "Terça-feira"; 
+                alert(day); 
+                break;
+            case 3:
+                day = "Quarta-feira"; 
+                alert(day); 
+                break;
+            case 4:
+                day = "Quinta-feira"; 
+                alert(day); 
+                break;
+            case 5:
+                day = "Sexta-feira"; 
+                alert(day); 
+                break;
+            case 6:
+                day = "Sábado";
+                alert(day); 
+                break;
         }
