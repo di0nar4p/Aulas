7bc3b237370658d8fc695a0ddca410eab2d46179
di0nar4p <glaucoplay@live.com>
2023-02-27 11:13:45 -0300

    Segunda aula de JS

--- Javascript/aula2/scripts.js
+++ Javascript/aula2/scripts.js
@@ -1,3 +1,12 @@
-//Condicionais
-let x= prompt("Digite seu nome")
-document.getElementById("teste").innerHTML = x;
+//Aula de Condicionais
+//let x= prompt("Digite seu nome")
+//document.getElementById("teste").innerHTML = x;
+
+//Exemplo de condicional
+let numero =prompt("Digite um número");
+        if(numero%2 == 0){
+            alert(numero +" é par");
+        }
+        else{
+            alert(numero +" é ímpar");
+        }
